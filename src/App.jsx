e64de1c5a3e42c957b0ea4d8f5ce5bd46b0098de
Nickss09 <nicoliemuller0iap@gmail.com>
2025-05-20 16:29:55 -0300

    Commit dos components

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <h1 className="titulo">Tarefas</h1>
+        <div className="checklist">
+          <p className="t1">1</p>
+          <p className="t2">2</p>
+          <p className="t3">3</p>
+          <p className="t4">4</p>
+          <div className="adicionarTarefa"> </div>
+        </div>
+    </>
+  )
+}
+
+export default App
