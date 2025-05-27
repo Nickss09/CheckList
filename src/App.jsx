76cad159f7bb8a7395be6e576e5701eb77707d80
Nickss09 <nicoliemuller0iap@gmail.com>
2025-05-27 13:47:21 -0300

    Ajustes no funcionamento

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,48 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
+import List from "./components/List";
 
 function App() {
+  const [tasks, setTasks] = useState([
+  ]);
+
+  const [newTask, setNewTask] = useState("");
+
+  const addTask = () => {
+    if (newTask !== "") {
+      setTasks([...tasks, newTask]);
+      setNewTask("");
+    }
+  };
 
   return (
     <>
-        <h1 className="titulo">Tarefas</h1>
-        <div className="checklist">
-          <p className="t1">1</p>
-          <p className="t2">2</p>
-          <p className="t3">3</p>
-          <p className="t4">4</p>
-          <div className="adicionarTarefa"> </div>
+      <div className="div">
+        <div className="Header">
+          <h2 className="Title">Lista de Tarefas</h2>
         </div>
+
+        <div className="List">
+          {tasks.map((task, index) => (
+            <div className="Tarefas" key={index}>
+              <h3>
+                <List id={`cbx-${index}`} label={task} />
+              </h3>
+            </div>
+          ))}
+        </div>
+
+        <input className="Caixa_de_Texto"
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          placeholder="Digite aqui"
+        />
+
+        <button onClick={addTask}>Adicionar Tarefa</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
